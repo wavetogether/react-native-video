aa1faa78af81857ef587dfb345eb2be6cf9312d6
Ryan Lee <12857462+WaveRyan@users.noreply.github.com>
2021-09-01 01:06:29 +0900

    Update ReactExoplayerView.java
    add eventEmitter.playbackRateChange(playWhenReady && playbackState == ExoPlayer.STATE_READY ? 1.0f : 0.0f);

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -736,6 +736,8 @@ class ReactExoplayerView extends FrameLayout implements
     @Override
     public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
         String text = "onStateChanged: playWhenReady=" + playWhenReady + ", playbackState=";
+        eventEmitter.playbackRateChange(playWhenReady && playbackState == ExoPlayer.STATE_READY ? 1.0f : 0.0f);
+        
         switch (playbackState) {
             case Player.STATE_IDLE:
                 text += "idle";
